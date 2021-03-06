f8937f03d55f83f079aaf459d1793768cfac3b41
Nathanael Cruz <nathanael.cruz.fd@gmail.com>
2021-03-05 21:24:05 -0300

    Structured successfully

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>CAV TARÔ | Tire sua sorte como cavaleiro sagrado</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    </div>
+  )
+}
